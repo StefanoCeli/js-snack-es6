2c0876f8318dfd6b09232c4e6d51df68cbce4f27
StefanoCeli <stefanopsn91@gmail.com>
2021-05-03 18:18:08 +0200

    fine snack4

---
+++ snack4/assets/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+   //creo un array di animali
+   const persone = [
+       {
+           nome : "Gianluca",
+           cognome : "Bianchi",
+           eta : 17
+       },
+       {
+           nome : "Pietro",
+           cognome : "Rossi",
+           eta : 35
+       },
+       {
+           nome : "Alberta",
+           cognome : "De Santis",
+           eta : 27
+       },
+       {
+           nome : "Daniele",
+           cognome : "Verdi",
+           eta : 12
+       },
+       {
+           nome : "Giuseppe",
+           cognome : "Casadei",
+           eta : 9
+       },
+       {
+           nome : "Francesco",
+           cognome : "Totti",
+           eta : 44
+       },
+       {
+           nome : "Greta",
+           cognome : "Trombetta",
+           eta : 15
+       }
+       
+   ];
+   console.log(persone);
+
+   persone.forEach((persona)=>{
+    let {nome,cognome,eta}=persona;
+    let idoneita = (eta>18) ? 'Può guidare' : 'Non può guidare'
+
+    let risultato = `
+                    ${nome} ${cognome} ${idoneita}
+    `
+    console.log(risultato);
+    console.log('--------------------------------------------------------------------');
+   })
+  
+
+
+});
+
+   
+    
+   
